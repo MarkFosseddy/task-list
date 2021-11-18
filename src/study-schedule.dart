71f190f67d642253e600cd26dedd7106b231850c
Mark Fosseddy <markfosseddy@gmail.com>
2021-11-18 21:23:45 +0500

    code resructure

--- src/study-schedule.dart
+++ src/study-schedule.dart
@@ -1,109 +1,193 @@
 import "dart:io";
 
 void main() {
+    Program().run();
+}
+
+class Program {
     bool exit = false;
-    String cmd = "";
+    Command cmd = Command("");
     String message = "";
 
-    var morning = Schedule([ScheduleItem("Hello, World!", true)]);
-    var afternoon = Schedule([ScheduleItem("How Are You", false)]);
-    var evening = Schedule([ScheduleItem("This is Me", false)]);
+    List<ScheduleItem> morning = [ScheduleItem(0, "Hello, World!", "")];
+    List<ScheduleItem> afternoon = [ScheduleItem(0, "How Are You", "Optional description text")];
+    List<ScheduleItem> evening = [ScheduleItem(0, "This is Me", "")];
 
-    while (!exit) {
-        // draw
-        ClearScreen();
+    void run() {
+        while (!exit) {
+            draw();
+            update();
+        }
+    }
 
-        Text("┏━━ Study Schedule ━━━\n");
-        EmptyLine();
+    void draw() {
+        UI.ClearScreen();
 
-        Text("  Morning:\n");
-        ListView(morning.items.length, (int i) {
-            var item = morning.items.elementAt(i);
-            Text("    [${item.completed ? "X" : " "}] ${item.text}\n");
+        UI.Text("┏━━ Study Schedule ━━━\n");
+        UI.EmptyLine();
+
+        UI.Text("  Morning:\n");
+        UI.ListView(morning.length, (int i) {
+            var item = morning.elementAt(i);
+            UI.Text("    ${item.title}\n");
+            if (item.desc.isNotEmpty) {
+                UI.Text("      ${item.desc}\n");
+                UI.EmptyLine();
+            } else {
+                UI.EmptyLine();
+            }
         });
 
-        EmptyLine();
+        UI.EmptyLine();
 
-        Text("  Afternoon:\n");
-        ListView(afternoon.items.length, (int i) {
-            var item = afternoon.items.elementAt(i);
-            Text("    [${item.completed ? "X" : " "}] ${item.text}\n");
+        UI.Text("  Afternoon:\n");
+        UI.ListView(afternoon.length, (int i) {
+            var item = afternoon.elementAt(i);
+            UI.Text("    ${item.title}\n");
+            if (item.desc.isNotEmpty) {
+                UI.Text("      ${item.desc}\n");
+                UI.EmptyLine();
+            } else {
+                UI.EmptyLine();
+            }
         });
 
-        EmptyLine();
+        UI.EmptyLine();
 
-        Text("  Evening:\n");
-        ListView(evening.items.length, (int i) {
-            var item = evening.items.elementAt(i);
-            Text("    [${item.completed ? "X" : " "}] ${item.text}\n");
+        UI.Text("  Evening:\n");
+        UI.ListView(evening.length, (int i) {
+            var item = evening.elementAt(i);
+            UI.Text("    ${item.title}\n");
+            if (item.desc.isNotEmpty) {
+                UI.Text("      ${item.desc}\n");
+                UI.EmptyLine();
+            } else {
+                UI.EmptyLine();
+            }
         });
 
-        EmptyLine();
-        EmptyLine();
+        UI.EmptyLine();
+        UI.EmptyLine();
+
+        UI.Text("cmd-name: ${cmd.name}\n");
+        UI.Text("cmd-args: ${cmd.args}\n");
+        UI.EmptyLine();
 
         if (message.isNotEmpty) {
-            Text(message);
+            UI.Text(message);
         } else {
-            EmptyLine();
+            UI.EmptyLine();
         }
 
-        Text(">> ");
+        UI.Text(">> ");
+    }
 
-        // update
+    void update() {
         message = "";
 
-        cmd = stdin.readLineSync() ?? "";
-        cmd = cmd.trim().toLowerCase();
-        if (cmd.isEmpty) continue;
+        cmd = Command(stdin.readLineSync() ?? "");
+        if (cmd.name.isEmpty) return;
 
-        switch (cmd) {
-            case "add":
-                break;
+        switch (cmd.name) {
+            case "add": {
+                if (cmd.args.isEmpty) {
+                    message = "Not enough arguments for `add` command\n";
+                    break;
+                }
+
+                if (!["m", "a", "e"].contains(cmd.args.first)) {
+                    message = "Unknown argument `${cmd.args.first}` for `add` command\n";
+                    break;
+                }
+
+                String title = "";
+                String desc = "";
+
+                stdout.write("Title: ");
+                title = stdin.readLineSync() ?? "";
+                title = title.trim();
+
+                if (title.isEmpty) break;
+
+                stdout.write("Description (optional):\n");
+                desc = stdin.readLineSync() ?? "";
+                desc = desc.trim();
+
+                var item = ScheduleItem(
+                    DateTime.now().microsecondsSinceEpoch,
+                    title,
+                    desc
+                );
+
+                switch (cmd.args.first) {
+                    case "m":
+                        morning.add(item);
+                        break;
+
+                    case "a":
+                        afternoon.add(item);
+                        break;
+
+                    case "e":
+                        evening.add(item);
+                        break;
+                }
+            } break;
 
             case "q":
+            case "quit":
+            case "exit":
                 exit = true;
-                ClearScreen();
+                UI.ClearScreen();
                 break;
 
-            default: message = "Unknown command: `$cmd`\n";
+            default: message = "Unknown command: `${cmd.name}`\n";
         }
     }
 }
 
-// @TODO: load items from file
-class Schedule {
-    List<ScheduleItem> items;
+class Command {
+    String name = "";
+    List<String> args = [];
 
-    Schedule(this.items);
+    Command(String cmd) {
+        cmd = cmd.trim().toLowerCase();
+
+        if (cmd.isEmpty) return;
+
+        List<String> parts = cmd.split(" ")
+            .where((x) => x.isNotEmpty)
+            .toList();
+
+        this.name = parts.first;
+
+        parts.removeAt(0);
+        this.args = parts;
+    }
 }
 
 class ScheduleItem {
-    String text;
-    bool completed;
+    int id;
+    String title;
+    String desc;
 
-    ScheduleItem(this.text, this.completed);
+    ScheduleItem(this.id, this.title, this.desc);
 }
 
-class Text {
-    Text(String text) {
-        stdout.write(text);
-    }
-}
-
-class EmptyLine {
-    EmptyLine() {
-        Text("\n");
-    }
-}
-
-class ClearScreen {
-    ClearScreen() {
+class UI {
+    UI.ClearScreen() {
         stdout.write(Process.runSync("clear", []).stdout);
     }
-}
 
-class ListView {
-    ListView(int listLen, Function(int i) builder) {
+    UI.Text(String text) {
+        stdout.write(text);
+    }
+
+    UI.EmptyLine() {
+        UI.Text("\n");
+    }
+
+    UI.ListView(int listLen, Function(int i) builder) {
         for (int i = 0; i < listLen; i++) {
             builder(i);
         }
