17c9cc4d486bb2428757aa8089ee9e5e6eb9a98c
Mark Fosseddy <markfosseddy@gmail.com>
2021-12-26 21:03:05 +0500

    organize program loop

--- src/study-schedule.go
+++ src/study-schedule.go
@@ -8,10 +8,42 @@ import (
 
 func main() {
     exit := false
+    message := ""
 
     for !exit {
+        // draw
         clearScreen()
-        fmt.Println("hello, world")
+
+        fmt.Println("┏━━ Study Schedule ━━━");
+        fmt.Println();
+
+        fmt.Println(message);
+
+        fmt.Print(">> ");
+
+        // update
+        message = "";
+
+        cmd := ""
+        fmt.Scanln(&cmd)
+        if len(cmd) == 0 {
+            continue
+        }
+
+        switch cmd {
+        case "add","ad","a":
+            message = "TODO: Add new Item"
+
+        case "delete", "del", "d":
+            message = "TODO: Delete Item"
+
+        case "q", "quit", "exit":
+            exit = true
+            clearScreen()
+
+        default:
+            message = fmt.Sprintf("Unknown command: `%s`", cmd);
+        }
     }
 }
 
