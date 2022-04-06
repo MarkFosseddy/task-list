d8f1e941bb5c6212aeac4c0469d6304c5ff9f443
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-06 12:17:27 +0500

    add `add` command

--- task-list.go
+++ task-list.go
@@ -7,36 +7,72 @@ import (
     "strings"
 )
 
+type Task struct {
+    id int
+    title string
+    description string
+}
+
+func draw(tasks []Task, message string) {
+    fmt.Print("\033c")
+
+    fmt.Println("Task List")
+    fmt.Println(tasks)
+    fmt.Println()
+
+    if len(message) > 0 {
+        fmt.Println(message)
+    } else {
+        fmt.Println()
+    }
+
+    fmt.Print(">> ")
+}
+
 func main() {
+    id := 1
     exit := false
     message := ""
+    tasks := []Task{}
 
     input := bufio.NewScanner(os.Stdin)
 
     for !exit {
-        fmt.Print("\033c")
-
-        fmt.Println("Task List")
-        fmt.Println()
-
-        if len(message) > 0 {
-            fmt.Println(message)
-        } else {
-            fmt.Println()
-        }
-
-        fmt.Print(">> ")
-
         message = ""
+
+        draw(tasks, message)
 
         input.Scan()
         cmd := strings.Trim(input.Text(), " ")
-
         if len(cmd) == 0 { continue }
 
         switch cmd {
         case "q":
             exit = true
+
+        case "add": {
+            message = "Enter title (empty to cancel):"
+            draw(tasks, message)
+
+            input.Scan()
+            title := strings.Trim(input.Text(), " ")
+            if len(title) == 0 { continue }
+
+            message = "Enter description (optional):"
+            draw(tasks, message)
+
+            input.Scan()
+            desc := strings.Trim(input.Text(), " ")
+
+            t := Task{}
+            t.id = id
+            t.title = title
+            t.description = desc
+
+            tasks = append(tasks, t)
+
+            id += 1
+        }
 
         default:
             message = fmt.Sprintf("Unknown command `%s`", cmd)
