e484d46a7a55c7ef80f991e542511ec383331ece
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-07 16:27:16 +0500

    clean

--- task-list.go
+++ task-list.go
@@ -7,46 +7,28 @@ import (
 	"strings"
 )
 
-type State struct {
-	tasks   []Task
-	message string
-}
-
-type Task struct {
-	id          int
-	title       string
-	description string
-}
-
-func draw(s *State) {
-	fmt.Print("\033c");
-
-	fmt.Println("Task List")
-	fmt.Println(s.tasks)
-	fmt.Println()
-
-	if len(s.message) > 0 {
-		fmt.Println(s.message)
-	} else {
-		fmt.Println()
-	}
-
-	fmt.Print(">> ")
-}
-
 func main() {
-	state := &State{}
-	state.message = ""
-	state.tasks = []Task{}
-
-	id := 1
 	exit := false
-
+	msg := ""
 	input := bufio.NewScanner(os.Stdin)
 
 	for !exit {
-		state.message = ""
-		draw(state)
+		// draw
+		fmt.Print("\033c")
+
+		fmt.Println("Task List")
+		fmt.Println()
+
+		if len(msg) > 0 {
+			fmt.Println(msg)
+		} else {
+			fmt.Println()
+		}
+
+		fmt.Print(">> ")
+
+		// update
+		msg = ""
 
 		input.Scan()
 		cmd := strings.Trim(input.Text(), " ")
@@ -58,34 +40,8 @@ func main() {
 		case "q":
 			exit = true
 
-		case "add": {
-			state.message = "Enter title (empty to cancel):"
-			draw(state)
-
-			input.Scan()
-			title := strings.Trim(input.Text(), " ")
-			if len(title) == 0 {
-				continue
-			}
-
-			state.message = "Enter description (optional):"
-			draw(state)
-
-			input.Scan()
-			desc := strings.Trim(input.Text(), " ")
-
-			t := Task{}
-			t.id = id
-			t.title = title
-			t.description = desc
-
-			state.tasks = append(state.tasks, t)
-
-			id += 1
-		}
-
 		default:
-			state.message = fmt.Sprintf("Unknown command `%s`", cmd)
+			msg = fmt.Sprintf("Unknown command `%s`", cmd)
 		}
 	}
 }
