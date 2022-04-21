9b0ccde86e5014db96715892853d5fa40f4efd85
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-21 20:06:33 +0500

    clean up

--- task-list.go
+++ task-list.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -14,14 +15,6 @@ type cmdInput struct {
 	s *bufio.Scanner
 }
 
-func (i *cmdInput) scan() bool {
-	return i.s.Scan()
-}
-
-func (i *cmdInput) text() string {
-	return strings.TrimSpace(i.s.Text())
-}
-
 type task struct {
 	title string
 	desc  string
@@ -31,22 +24,35 @@ type storage struct {
 	file string
 }
 
-func (s *storage) read() []task {
-	fd, err := os.OpenFile(s.file, os.O_RDONLY|os.O_CREATE, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fd.Close()
+type application struct {
+	input    *cmdInput
+	tasks    []task
+	message  string
+	editing  bool
+	deleting bool
+}
 
-	bytes, err := io.ReadAll(fd)
-	if err != nil {
-		log.Fatal(err)
-	}
+func (i *cmdInput) scan() bool {
+	return i.s.Scan()
+}
+
+func (i *cmdInput) text() string {
+	return strings.TrimSpace(i.s.Text())
+}
+
+func (s *storage) read() []task {
+	f, err := os.OpenFile(s.file, os.O_RDONLY|os.O_CREATE, 0644)
+	assert(err == nil, err)
+	defer f.Close()
+
+	bytes, err := io.ReadAll(f)
+	assert(err == nil, err)
 
 	data := strings.TrimSpace(string(bytes))
 	items := []task{}
 
 	if len(data) > 0 {
+		err := ""
 		for _, line := range strings.Split(data, "\n") {
 			if title, desc, found := strings.Cut(line, " <-$-> "); found {
 				if desc == "-$-" {
@@ -54,40 +60,48 @@ func (s *storage) read() []task {
 				}
 				items = append(items, task{title, desc})
 			} else {
-				message += fmt.Sprintf("Error parsing task: %s\n", line)
+				err += fmt.Sprintf("Error parsing task: %s\n", line)
 			}
 		}
+		assert(err == "", errors.New(err))
 	}
 
 	return items
 }
 
 func (s *storage) write(data []task) {
-	fd, err := os.OpenFile(s.file, os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fd.Close()
+	f, err := os.OpenFile(s.file, os.O_WRONLY|os.O_TRUNC, 0644)
+	assert(err == nil, err)
+	defer f.Close()
 
 	buf := ""
 	for _, item := range data {
 		desc := item.desc
-		if len(desc) == 0 {
+		if desc == "" {
 			desc = "-$-"
 		}
 		buf += fmt.Sprintf("%s <-$-> %s\n", item.title, desc)
 	}
 
-	_, err = fd.WriteString(buf)
-	if err != nil {
+	_, err = f.WriteString(buf)
+	assert(err == nil, err)
+}
+
+func assert(cond bool, err error) {
+	if !cond {
 		log.Fatal(err)
 	}
 }
 
-var message string = ""
-var tasks []task = []task{}
-var deleting bool = false
-var updating bool = false
+var store storage = storage{"/home/fosseddy/.task-list"}
+
+var app application = application{
+	input:    &cmdInput{bufio.NewScanner(os.Stdin)},
+	tasks:    []task{},
+	message:  "",
+	editing:  false,
+	deleting: false,
+}
 
 func draw() {
 	// @NOTE(art): clear screen
@@ -96,8 +110,8 @@ func draw() {
 	fmt.Println("Task List")
 	fmt.Println()
 
-	for i, t := range tasks {
-		if deleting || updating {
+	for i, t := range app.tasks {
+		if app.deleting || app.editing {
 			fmt.Printf(
 				"  [%v] title: %v\n  description: %v\n\n",
 				i+1, t.title, t.desc,
@@ -107,8 +121,8 @@ func draw() {
 		}
 	}
 
-	if len(message) > 0 {
-		fmt.Println(message)
+	if app.message != "" {
+		fmt.Println(app.message)
 	} else {
 		fmt.Println()
 	}
@@ -117,61 +131,58 @@ func draw() {
 }
 
 func main() {
-	store := storage{"/home/fosseddy/.task-list"}
-	tasks = store.read()
-
+	app.tasks = store.read()
 	exit := false
-	input := cmdInput{bufio.NewScanner(os.Stdin)}
 
 	for !exit {
 		draw()
 
-		message = ""
-		input.scan()
-		cmd := input.text()
-		if len(cmd) == 0 {
+		app.message = ""
+		app.input.scan()
+		cmd := app.input.text()
+		if cmd == "" {
 			continue
 		}
 
 		switch cmd {
 		case "add":
-			message = "Enter title (empty to cancel):"
+			app.message = "Enter title (empty to cancel):"
 			draw()
-			input.scan()
-			title := input.text()
+			app.input.scan()
+			title := app.input.text()
 
-			if len(title) > 0 {
-				message = "Enter description (optional):"
+			if title != "" {
+				app.message = "Enter description (optional):"
 				draw()
-				input.scan()
-				desc := input.text()
-				tasks = append(tasks, task{title, desc})
+				app.input.scan()
+				desc := app.input.text()
+				app.tasks = append(app.tasks, task{title, desc})
 			}
 
-			store.write(tasks)
+			store.write(app.tasks)
 
-			message = ""
+			app.message = ""
 		case "delete":
-			if len(tasks) == 0 {
-				message = "You have no tasks. Use `add` command to create one"
+			if len(app.tasks) == 0 {
+				app.message = "You have no tasks. Use `add` command to create one"
 				break
 			}
 
-			message = "Choose task to delete (empty to cancel):"
-			deleting = true
+			app.message = "Choose task to delete (empty to cancel):"
+			app.deleting = true
 			id := -1
 
 			for {
 				draw()
-				input.scan()
-				text := input.text()
+				app.input.scan()
+				text := app.input.text()
 
-				if len(text) == 0 {
+				if text == "" {
 					break
 				}
 
 				if val, err := strconv.Atoi(text); err == nil {
-					if val >= 1 && val <= len(tasks) {
+					if val >= 1 && val <= len(app.tasks) {
 						// @NOTE(art): in ui we draw ids starting from 1
 						id = val - 1
 						break
@@ -181,38 +192,38 @@ func main() {
 
 			if id != -1 {
 				var tmp []task
-				for i, t := range tasks {
+				for i, t := range app.tasks {
 					if i != id {
 						tmp = append(tmp, t)
 					}
 				}
-				tasks = tmp
-				store.write(tasks)
+				app.tasks = tmp
+				store.write(app.tasks)
 			}
 
-			deleting = false
-			message = ""
-		case "update":
-			if len(tasks) == 0 {
-				message = "You have no tasks. Use `add` command to create one"
+			app.deleting = false
+			app.message = ""
+		case "edit":
+			if len(app.tasks) == 0 {
+				app.message = "You have no tasks. Use `add` command to create one"
 				break
 			}
 
-			message = "Choose task to update (empty to cancel):"
-			updating = true
+			app.message = "Choose task to edit (empty to cancel):"
+			app.editing = true
 			id := -1
 
 			for {
 				draw()
-				input.scan()
-				text := input.text()
+				app.input.scan()
+				text := app.input.text()
 
-				if len(text) == 0 {
+				if text == "" {
 					break
 				}
 
 				if val, err := strconv.Atoi(text); err == nil {
-					if val >= 1 && val <= len(tasks) {
+					if val >= 1 && val <= len(app.tasks) {
 						// @NOTE(art): in ui we draw ids starting from 1
 						id = val - 1
 						break
@@ -221,41 +232,41 @@ func main() {
 			}
 
 			if id != -1 {
-				message = "Enter new title (empty to skip):"
+				app.message = "Enter new title (empty to skip):"
 				draw()
-				input.scan()
-				title := input.text()
+				app.input.scan()
+				title := app.input.text()
 
-				message = "Enter new description (empty to skip):"
+				app.message = "Enter new description (empty to skip):"
 				draw()
-				input.scan()
-				desc := input.text()
+				app.input.scan()
+				desc := app.input.text()
 
 				changed := false
 
-				if len(title) > 0 {
-					tasks[id].title = title
+				if title != "" {
+					app.tasks[id].title = title
 					changed = true
 				}
 
-				if len(desc) > 0 {
-					tasks[id].desc = desc
+				if desc != "" {
+					app.tasks[id].desc = desc
 					changed = true
 				}
 
 				if changed {
-					store.write(tasks)
+					store.write(app.tasks)
 				}
 			}
 
-			updating = false
-			message = ""
+			app.editing = false
+			app.message = ""
 		case "exit":
 			exit = true
 		case "help":
-			message = "add\ndelete\nupdate\nhelp\nexit\n"
+			app.message = "add\ndelete\nedit\nhelp\nexit\n"
 		default:
-			message = fmt.Sprintf(
+			app.message = fmt.Sprintf(
 				"Unknown command `%s`. Type `help` to see commands",
 				cmd,
 			)
